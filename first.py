2fc0bbb777c58f6e6fc52aa7f6170755110666d0
ayushdeshpande <ayushdeshpande360@gmail.com>
2026-09-22 10:31:01 +0530

    ver.py

---
+++ first.py
@@ -0,0 +1,18 @@
+class vector2D:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+    def add(self,v):
+       return vector2D(self.x + v.x, self.y + v.y)
+    def display(self):
+         print("(", self.x, ",", self.y,")")    
+         
+v1 = vector2D(2,3)
+v2 = vector2D(4,5)
+v3 = v1.add(v2)
+
+v3.display()              
+    
+    
+    
